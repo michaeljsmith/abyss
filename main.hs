6ecfce8448bd1fe1c80562cbb48484d4dc8956de
Michael Smith <msmith@msmith.id.au>
2011-10-04 08:36:37 +1100

    Restart

--- main.hs
+++ main.hs
@@ -8,89 +8,24 @@ instance Applicative (State a) where
   pure  = return
   (<*>) = ap
 
-type Block a = State [String] a
-type Code = Block String
+type Cpp a = State [Label] a
+newtype Label = Label {labelString :: String}
 
-instance Monoid Code where
-  mappend = liftA2 (++)
-  mempty = code ""
+newLabel :: Cpp Label
+newLabel = gets head <* (modify tail)
 
-generateCode :: Code -> String
-generateCode block =
-  evalState block ["v" ++ show i | i <- [1..]]
+newtype Literal a = Literal a
+newtype Expression a = Expression String
+newtype Block = Block String
 
-code :: String -> Code
-code code = return code
+blockplus :: Block -> Block -> Block
+blockplus (Block s0) (Block s1) = Block (s0 ++ s1)
 
-statement :: String -> String
-statement code = code ++ ";\n"
-
-eval :: Code -> Code
-eval expr = statement <$> expr
-
-assign :: Variable -> Code -> Code
-assign var expr = statement <$> (setValue var expr)
-
-newtype Label = Label {getLabelString :: String}
-
-newLabel :: Block Label
-newLabel = Label <$> (gets head <* (modify tail))
-
-data FunctionDeclarationBuilder a = FunctionDeclarationBuilder a Code
-
-parameter :: Variable
-parameter = Variable_ <$> newLabel
-
-declareParam :: Variable -> Code
-declareParam var = getValue var
-
-addParameter :: FunctionDeclarationBuilder (Variable -> b) -> Variable -> FunctionDeclarationBuilder b
-addParameter (FunctionDeclarationBuilder body vars) var =
-  FunctionDeclarationBuilder (body var) (vars `mappend` declareParam var)
-
-signature :: a -> FunctionDeclarationBuilder a
-signature a = FunctionDeclarationBuilder a (code "")
-
-function :: FunctionDeclarationBuilder Code -> Code
-function (FunctionDeclarationBuilder body decls) =
-  code "function (" `mappend` decls `mappend` code ")\n{\n" `mappend` body `mappend` code "}\n"
-
-class Gettable g where
-  getValue :: g -> Code
-
-class Settable s where
-  setValue :: s -> Code -> Code
-
-data Variable_ = Variable_ {getVarName_ :: Label}
-type Variable = Block Variable_
-
-instance Gettable Variable where
-  getValue var = getLabelString <$> (getVarName_ <$> var)
-
-instance Settable Variable where
-  setValue var expr = getValue var `mappend` code " = " `mappend` expr
-
-variable :: Variable
-variable = Variable_ <$> newLabel
-
-withVar :: (Variable -> Block a) -> Block a
-withVar fn = fn variable
-
-prompt :: (Gettable g) => g -> Code
-prompt message = code "print(" `mappend` (getValue message) `mappend` code ")"
-
-readInput :: Code
-readInput = code "read()"
-
-appBlock :: Variable -> Code
-appBlock prm =
-  eval (prompt prm) `mappend`
-  assign prm readInput
-
-fun :: Code
-fun = function (signature appBlock `addParameter` parameter)
-
-appCode = generateCode fun
+withVar :: Literal a -> Cpp (Expression a -> Block) -> Cpp Block
+withVar (Literal x) blockTpl =
+  (\f x -> Block ("var " ++ x ++ "\n") `blockplus` f x) <$>
+    (pure (.) <*> blockTpl <*> pure Expression) <*>
+    (labelString <$> newLabel)
 
 main =
-  putStr appCode
+  putStrLn "hello"
