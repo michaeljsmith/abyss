592aaf1591f6ead545864a7b219269ad7a873097
Michael Smith <msmith@msmith.id.au>
2011-10-13 11:09:35 +1100

    FUnction progress.

--- main.hs
+++ main.hs
@@ -13,13 +13,45 @@ instance Applicative Code where
                                (a, st2) = runCode sv st1
                            in (f a, st2))
 
+class Type t where
+  typeCode :: t -> Code ()
+
 code :: String -> Code ()
 code s = Code $ \s0 -> ((), s0 ++ s)
 
 execCode :: Code a -> String
 execCode c = snd $ runCode c ""
 
-program = code "foo"
+scope :: Code a -> Code ()
+scope body = code "{\n" *> body *> code "\n}\n" 
+
+newtype Function f = Function {runFunc :: (f, Code ())}
+funDecl = snd . runFunc
+funInvoke = fst . runFunc
+
+function type_ name (invoke, body) = Function (code (name ++ "(") *> invoke *> code ")", typeCode type_ *> code " " *> code name *> body)
+
+body type_ block = (pure type_, scope block)
+
+data Int_ = Int_
+int = Int_
+
+instance Type Int_ where
+  typeCode t = code "int"
+
+fooDecl = function int "foo"$
+  body int$
+    code "return 1;"
+foo = funInvoke fooDecl
+
+barDecl = function int "bar"$
+  body int$
+    code "printf(\"%d\\n\", " *> foo *> code ");"
+bar = funInvoke barDecl
+
+program =
+  funDecl fooDecl *>
+  funDecl barDecl
 
 main = putStrLn programText
   where
