ec3410f2c0663c4429e3c88e8b20313b2d73032d
Michael Smith <msmith@msmith.id.au>
2011-10-22 17:10:53 +1100

    Progress

--- main.hs
+++ main.hs
@@ -18,7 +18,10 @@ arg :: a :-> b -> a
 arg (a :-> b) = a
 
 subst :: (Inst x :-> Inst a :-> Inst b) -> (Inst x :-> Inst a) -> (Inst x :-> Inst b)
-subst f g = Pair (arg f) (arg g) :-> (res f :* res g)
+subst f g = Pair (arg f) (arg g) :-> ((f :* x1) :* (g :* x0))
+  where
+    x0 = Pair x (arg f)
+    x1 = Pair x (arg g)
 
 main = print "hello"
 
