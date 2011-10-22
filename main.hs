15ecc76040655497b914a7d94a3a3065607c4a64
Michael Smith <msmith@msmith.id.au>
2011-10-22 23:00:52 +1100

    pair

--- main.hs
+++ main.hs
@@ -4,17 +4,29 @@ import Control.Applicative
 import Data.Typeable
 import Data.Dynamic
 
-data Inst a where
-  (:->) :: Inst a -> Inst b -> Inst (a -> b)
-  S :: Inst (x -> a -> b) -> Inst (x -> a) -> Inst (x -> b)
-  K :: Inst a -> Inst (x -> a)
-  I :: Inst (a -> a)
-
-  Inst :: a -> Inst a
-  Pair :: Inst a -> Inst a -> Inst a
-
-(|*|) :: Inst (a -> b) -> Inst a -> Inst b
+data a :-> b where
+  (:->) :: [a] -> ([a] -> b) -> (a :-> b)
 
 infixr 1 :->
+
+refs :: (a :-> b) -> [a]
+refs (a :-> b) = a
+
+gen :: (a :-> b) -> [a] -> b
+gen (a :-> b) = b
+
+(|*|) :: (a :-> b) -> a -> b
+f |*| x = gen f [x]
+
+sbst :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
+sbst f g = newRefs :-> genOutput
+  where
+    newRefs = refs f ++ refs g
+    genOutput = \xs -> subf xs |*| subg xs
+    subf xs = (gen f) (refs g ++ xs)
+    subg xs = (gen g) (refs f ++ xs)
+
+cnst :: a -> (x :-> a)
+cnst a = [] :-> (\x -> a)
 
 main = print "hello"
