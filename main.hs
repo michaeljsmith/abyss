3df89e8b80ca68e44174f329c3e5da1c1a216da6
Michael Smith <msmith@msmith.id.au>
2011-10-22 20:41:06 +1100

    Silly

--- main.hs
+++ main.hs
@@ -4,25 +4,17 @@ import Control.Applicative
 import Data.Typeable
 import Data.Dynamic
 
-data a :-> b where
-  (:->) :: a -> b -> (a :-> b)
-  S :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
-  K :: a -> (x :-> a)
-  I :: (a :-> a)
-
-infixr 1 :->
-
 data Inst a where
+  (:->) :: Inst a -> Inst b -> Inst (a -> b)
+  S :: Inst (x -> a -> b) -> Inst (x -> a) -> Inst (x -> b)
+  K :: Inst a -> Inst (x -> a)
+  I :: Inst (a -> a)
+
   Inst :: a -> Inst a
   Pair :: Inst a -> Inst a -> Inst a
-  (:*) :: (Inst a :-> Inst b) -> Inst a -> Inst b
 
-(|*|) :: (Applicative f) => f (a -> b) -> Inst (f a) -> f b
-f |*| Inst x = f <*> x
-f |*| Pair x y = (f |*| x) *> (f |*| y)
---f |*| ((a :-> b) :* x) = 
---S :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
---K :: a -> (x :-> a)
---I :: (a :-> a)
+(|*|) :: Inst (a -> b) -> Inst a -> Inst b
+
+infixr 1 :->
 
 main = print "hello"
