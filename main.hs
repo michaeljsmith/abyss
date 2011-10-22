04704cb1b5157a20f4855c5d8cfd94416d94db55
Michael Smith <msmith@msmith.id.au>
2011-10-22 16:44:06 +1100

    Inst

--- main.hs
+++ main.hs
@@ -4,35 +4,14 @@ import Control.Applicative
 import Data.Typeable
 import Data.Dynamic
 
-data a :-> b where
-  (:->) :: a -> b -> (a :-> b)
-  Sbst :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
-  Ignr :: b -> a -> (b :-> a)
+data Inst a where
+  Inst :: a -> Inst a
+  Pair :: Inst a -> Inst a -> Inst a
 
-data Object a where
-  Object :: a -> Object a
-  (:*) :: (Object a :-> Object b) -> Object a -> Object b
-  ObjectPair :: Object a -> Object a -> Object a
+subst :: Inst (x -> a -> b) -> Inst (x -> a) -> Inst (x -> b)
+subst f g x = f x0 (g x1) where
+  x0 = Pair x (arg g)
+  x1 = Pair x (arg f)
 
-infixr 1 :->
-
-type Variable = IORef String
-
-memberO s m = do
-  ref <- newIORef s
-
---data Value a = Value {valueExpression :: a}
---textOf (Value a) = show a
-
---constant x = Value x
-
---data TextDisplay = TextDisplay (Value String)
---display (TextDisplay v) = print (textOf v)
-
---textDisplay v = v :-> TextDisplay v
-
---app = textDisplay (constant "Hello, world!")
-
---main = display app
 main = print "hello"
 
