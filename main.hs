7638718ee412f4bfa62a9c71fdd4e98b7ac52fdd
Michael Smith <msmith@msmith.id.au>
2011-10-22 17:00:56 +1100

    Appl

--- main.hs
+++ main.hs
@@ -4,14 +4,21 @@ import Control.Applicative
 import Data.Typeable
 import Data.Dynamic
 
+data a :-> b where
+  (:->) :: a -> b -> (a :-> b)
+
 data Inst a where
   Inst :: a -> Inst a
   Pair :: Inst a -> Inst a -> Inst a
+  (:*) :: (Inst a :-> Inst b) -> Inst a -> Inst b
 
-subst :: Inst (x -> a -> b) -> Inst (x -> a) -> Inst (x -> b)
-subst f g x = f x0 (g x1) where
-  x0 = Pair x (arg g)
-  x1 = Pair x (arg f)
+infixr 1 :->
+
+arg :: a :-> b -> a
+arg (a :-> b) = a
+
+subst :: (Inst x :-> Inst a :-> Inst b) -> (Inst x :-> Inst a) -> (Inst x :-> Inst b)
+subst f g = Pair (arg f) (arg g) :-> (res f :* res g)
 
 main = print "hello"
 
