5c666d9be1d3649203bee9cb3343a273a2928c5b
Michael Smith <msmith@msmith.id.au>
2011-10-22 17:24:05 +1100

    :-> + Inst

--- main.hs
+++ main.hs
@@ -6,22 +6,17 @@ import Data.Dynamic
 
 data a :-> b where
   (:->) :: a -> b -> (a :-> b)
+  S :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
+  K :: a -> (x :-> a)
+  I :: (a :-> a)
+
+infixr 1 :->
 
 data Inst a where
   Inst :: a -> Inst a
   Pair :: Inst a -> Inst a -> Inst a
   (:*) :: (Inst a :-> Inst b) -> Inst a -> Inst b
 
-infixr 1 :->
-
-arg :: a :-> b -> a
-arg (a :-> b) = a
-
-subst :: (Inst x :-> Inst a :-> Inst b) -> (Inst x :-> Inst a) -> (Inst x :-> Inst b)
-subst f g = Pair (arg f) (arg g) :-> ((f :* x1) :* (g :* x0))
-  where
-    x0 = Pair x (arg f)
-    x1 = Pair x (arg g)
+(|*|) :: (Inst a :-> Inst b) -> Inst a -> Inst b
 
 main = print "hello"
-
