42644fb482b20574f3ccd72268ae948fb6a55768
Michael Smith <msmith@msmith.id.au>
2011-10-23 19:28:04 +1100

    Progress

--- main.hs
+++ main.hs
@@ -5,28 +5,28 @@ import Data.Typeable
 import Data.Dynamic
 
 data a :-> b where
-  (:->) :: [a] -> ([a] -> b) -> (a :-> b)
+  Template :: [a] -> ([a] -> b) -> (a :-> b)
 
 infixr 1 :->
 
 refs :: (a :-> b) -> [a]
-refs (a :-> b) = a
+refs (Template a b) = a
 
 gen :: (a :-> b) -> [a] -> b
-gen (a :-> b) = b
+gen (Template a b) = b
 
 (|*|) :: (a :-> b) -> a -> b
 f |*| x = gen f [x]
 
 sbst :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
-sbst f g = newRefs :-> genOutput
+sbst f g = Template newRefs genOutput
   where
     newRefs = refs f ++ refs g
-    genOutput = \xs -> subf xs |*| subg xs
+    genOutput xs = subf xs |*| subg xs
     subf xs = (gen f) (refs g ++ xs)
     subg xs = (gen g) (refs f ++ xs)
 
 cnst :: a -> (x :-> a)
-cnst a = [] :-> (\x -> a)
+cnst a = Template [] (\x -> a)
 
 main = print "hello"
