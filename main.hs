6c382a397df5b85bb0342807e9164cfca19f202f
Michael Smith <msmith@msmith.id.au>
2011-10-23 19:34:02 +1100

    Monoid

--- main.hs
+++ main.hs
@@ -1,32 +1,33 @@
 {-#LANGUAGE GADTs, EmptyDataDecls, TypeSynonymInstances, TypeOperators, StandaloneDeriving, FlexibleContexts, DeriveDataTypeable, ScopedTypeVariables #-}
 
 import Control.Applicative
+import Data.Monoid
 import Data.Typeable
 import Data.Dynamic
 
 data a :-> b where
-  Template :: [a] -> ([a] -> b) -> (a :-> b)
+  Template :: a -> (a -> b) -> (a :-> b)
 
 infixr 1 :->
 
-refs :: (a :-> b) -> [a]
+refs :: (a :-> b) -> a
 refs (Template a b) = a
 
-gen :: (a :-> b) -> [a] -> b
+gen :: (a :-> b) -> a -> b
 gen (Template a b) = b
 
 (|*|) :: (a :-> b) -> a -> b
-f |*| x = gen f [x]
+f |*| x = gen f x
 
-sbst :: (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
+sbst :: Monoid x => (x :-> a :-> b) -> (x :-> a) -> (x :-> b)
 sbst f g = Template newRefs genOutput
   where
-    newRefs = refs f ++ refs g
+    newRefs = refs f `mappend` refs g
     genOutput xs = subf xs |*| subg xs
-    subf xs = (gen f) (refs g ++ xs)
-    subg xs = (gen g) (refs f ++ xs)
+    subf xs = (gen f) (refs g `mappend` xs)
+    subg xs = (gen g) (refs f `mappend` xs)
 
-cnst :: a -> (x :-> a)
-cnst a = Template [] (\x -> a)
+cnst :: Monoid x => a -> (x :-> a)
+cnst a = Template mempty (\x -> a)
 
 main = print "hello"
