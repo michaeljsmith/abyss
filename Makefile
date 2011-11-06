7678adf001c3e81312842baae5bb04d37c55f124
Michael Smith <msmith@msmith.id.au>
2011-11-06 18:19:41 +1100

    Progress.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
+.PHONEY: exe
+exe: a.out
+
 .PHONEY: run
 run: exe
 	./a.out
-
-.PHONEY: exe
-exe: a.out
 
 a.out: main.cpp
 	g++ -g -Wall -Wextra -Werror -std=c++0x -o a.out -lglfw -lGLU main.cpp
--- main.cpp
+++ main.cpp
@@ -1,31 +1,135 @@
 #include <GL/glfw.h>
 #include <stdlib.h>
-#include <GL/gl.h>	// Header File For The OpenGL32 Library
-#include <GL/glu.h>	// Header File For The GLu32 Library
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+#include <sys/time.h>
 
-/* A general OpenGL initialization function.  Sets all of the initial parameters. */
-void InitGL(int Width, int Height)	        // We call this right after our OpenGL window is created.
+using boost::function;
+using boost::shared_ptr;
+using boost::lambda::constant;
+using boost::lambda::var;
+using boost::lambda::bind;
+
+template <typename I, typename O>
+struct Relation
 {
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		// This Will Clear The Background Color To Black
-  glClearDepth(1.0);				// Enables Clearing Of The Depth Buffer
-  glDepthFunc(GL_LESS);				// The Type Of Depth Test To Do
-  glEnable(GL_DEPTH_TEST);			// Enables Depth Testing
-  glShadeModel(GL_SMOOTH);			// Enables Smooth Color Shading
+  Relation(I const& input, function<O (I)> const& generate_output):
+    input(input), generate_output(generate_output) {}
+  I input;
+  function<O (I)> generate_output;
+};
+
+template <typename I, typename O>
+O apply(Relation<I, O> const& f, I const& x) {
+  return f.generate_output(x);
+}
+
+template <typename T>
+struct ListNode;
+
+template <typename T>
+struct List : public shared_ptr<ListNode<T> const> {
+  List() {}
+  List(ListNode<T> const* ptr): shared_ptr<ListNode<T> const>(ptr) {}
+};
+
+template <typename T>
+struct ListNode {
+  ListNode(T const& head, List<T> const& tail): head(head), tail(tail) {}
+  T head;
+  List<T> tail;
+};
+
+template <typename T>
+T head(List<T> const& l) {
+  return l->head;
+}
+
+template <typename T>
+List<T> tail(List<T> const& l) {
+  return l->tail;
+}
+
+template <typename T>
+bool nullp(List<T> const& list) {
+  return list == 0;
+}
+
+template <typename T>
+List<T> cons(T const& x0, List<T> const& tail) {
+  List<T> l(new ListNode<T>(x0, tail));
+  return l;
+}
+
+template <typename T>
+List<T> list() {
+  return List<T>(0);
+}
+
+template <typename T>
+List<T> list(T const& x0) {
+  return cons(x0, list<T>());
+}
+
+template <typename T, typename F>
+void for_each(F const& f, List<T> l) {
+  for (List<T> rest = l; !nullp(rest); rest = tail(rest)) {
+    f(head(rest));
+  }
+}
+
+struct Float_
+{
+  Float_(function<void (float)> const& set): set(set) {}
+  function<void (float)> set;
+};
+typedef List<Float_> Float;
+
+void set(Float float_, float x) {
+  for_each(
+      bind(bind(&Float_::set, boost::lambda::_1), x),
+      float_);
+}
+
+struct SpriteData
+{
+  SpriteData(): pos(0.0f) {}
+  float pos;
+};
+typedef List<SpriteData*> Sprite;
+
+Relation<Float, Sprite> sprite(SpriteData* sprite) {
+  return Relation<Float, Sprite>(
+      Float(list(Float_(var(sprite->pos) = boost::lambda::_1))),
+      function<Sprite (Float)>(constant(list(sprite))));
+}
+
+void InitGL(int Width, int Height)
+{
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClearDepth(1.0);
+  glDepthFunc(GL_LESS);
+  glEnable(GL_DEPTH_TEST);
+  glShadeModel(GL_SMOOTH);
 
   glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();				// Reset The Projection Matrix
+  glLoadIdentity();
 
-  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);	// Calculate The Aspect Ratio Of The Window
+  gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);
 
   glMatrixMode(GL_MODELVIEW);
 }
 
 void GLFWCALL ReSizeGLScene(int Width, int Height)
 {
-  if (Height==0)				// Prevent A Divide By Zero If The Window Is Too Small
+  if (Height==0)
     Height=1;
 
-  glViewport(0, 0, Width, Height);		// Reset The Current Viewport And Perspective Transformation
+  glViewport(0, 0, Width, Height);
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -34,72 +138,74 @@ void GLFWCALL ReSizeGLScene(int Width, int Height)
   glMatrixMode(GL_MODELVIEW);
 }
 
-/* The main drawing function. */
-void DrawGLScene()
+void renderSprite(Sprite sprite)
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
-  glLoadIdentity();				// Reset The View
+  assert(!nullp(sprite));
 
-  glTranslatef(-1.5f,0.0f,-6.0f);		// Move Left 1.5 Units And Into The Screen 6.0
+  SpriteData* sprite_ = sprite->head;
+  glLoadIdentity();
+
+  glTranslatef(sprite_->pos, 0.0f, -6.0f);
 	
-  // draw a triangle (in smooth coloring mode)
-  glBegin(GL_POLYGON);				// start drawing a polygon
-  glColor3f(1.0f,0.0f,0.0f);			// Set The Color To Red
-  glVertex3f( 0.0f, 1.0f, 0.0f);		// Top
-  glColor3f(0.0f,1.0f,0.0f);			// Set The Color To Green
-  glVertex3f( 1.0f,-1.0f, 0.0f);		// Bottom Right
-  glColor3f(0.0f,0.0f,1.0f);			// Set The Color To Blue
-  glVertex3f(-1.0f,-1.0f, 0.0f);		// Bottom Left	
-  glEnd();					// we're done with the polygon (smooth color interpolation)	
-
-  glTranslatef(3.0f,0.0f,0.0f);		        // Move Right 3 Units
-
-  // draw a square (quadrilateral)
-  glColor3f(0.5f,0.5f,1.0f);			// set color to a blue shade.
-  glBegin(GL_QUADS);				// start drawing a polygon (4 sided)
-  glVertex3f(-1.0f, 1.0f, 0.0f);		// Top Left
-  glVertex3f( 1.0f, 1.0f, 0.0f);		// Top Right
-  glVertex3f( 1.0f,-1.0f, 0.0f);		// Bottom Right
-  glVertex3f(-1.0f,-1.0f, 0.0f);		// Bottom Left	
-  glEnd();					// done with the polygon
+  glBegin(GL_POLYGON);
+  glColor3f(1.0f, 0.0f, 0.0f);
+  glVertex3f(0.0f, 1.0f, 0.0f);
+  glColor3f(0.0f, 1.0f, 0.0f);
+  glVertex3f(1.0f, -1.0f,  0.0f);
+  glColor3f(0.0f, 0.0f, 1.0f);
+  glVertex3f(-1.0f, -1.0f, 0.0f);
+  glEnd();
 }
 
-int main( void )
-{
-  int running = GL_TRUE;
-  // Initialize GLFW
-  if( !glfwInit() )
-  {
-    exit( EXIT_FAILURE );
+struct Clock {
+  unsigned time;
+  Clock(): time(retrieve_time()) {}
+  unsigned get() {
+    return retrieve_time() - time;
   }
+
+private:
+  unsigned retrieve_time() {
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday(&tv, &tz);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  }
+};
+
+int main() {
+  bool running = true;
+  if (!glfwInit())
+    exit( EXIT_FAILURE );
 
   glfwSetWindowSizeCallback( ReSizeGLScene );
 
-  // Open an OpenGL window
-  if( !glfwOpenWindow( 300,300, 0,0,0,0,0,0, GLFW_WINDOW ) )
-  {
+  if (!glfwOpenWindow(300, 300, 0, 0, 0, 0, 32, 0, GLFW_WINDOW)) {
     glfwTerminate();
-    exit( EXIT_FAILURE );
+    exit(EXIT_FAILURE);
   }
 
-  /* Initialize our window. */
   InitGL(640, 480);
 
-  // Main loop
-  while( running )
-  {
-    // OpenGL rendering goes here...
-    glClear( GL_COLOR_BUFFER_BIT );
-    DrawGLScene();
+  SpriteData sprite_;
+  Relation<Float, Sprite> sprite_rel = sprite(&sprite_);
+  Float pos = sprite_rel.input;
+  Sprite sprite = apply(sprite_rel, Float());
 
-    // Swap front and back rendering buffers
+  Clock clock;
+  while (running) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    set(pos, clock.get() * 0.001f);
+    renderSprite(sprite);
+
     glfwSwapBuffers();
-    // Check if ESC key was pressed or window was closed
-    running = !glfwGetKey( GLFW_KEY_ESC ) &&
-      glfwGetWindowParam( GLFW_OPENED );
+    running =
+      !glfwGetKey(GLFW_KEY_ESC) &&
+      glfwGetWindowParam(GLFW_OPENED);
   }
-  // Close window and terminate GLFW
   glfwTerminate();
-  // Exit program
-  exit( EXIT_SUCCESS );
+
+  return 0;
 }
