9b304d0caae8a5d1bba0986b569a7138f1a196f1
Michael Smith <msmith@msmith.id.au>
2012-02-02 21:37:03 +1100

    FFI

--- Makefile
+++ Makefile
@@ -6,5 +6,6 @@ exe: $(EXE)
 .PHONEY: run
 run: exe
 	./a.out
+
 $(EXE): main.cpp
-	g++ -std=c++0x -o $(EXE) main.cpp
+	g++ -o $(EXE) -lffi main.cpp
--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <ffi/ffi.h>
 
 int main() {
-  printf("3\n");
+  ffi_cif cif;
+  ffi_type *args[1];
+  void *values[1];
+  char const *s;
+  int rc;
+
+  /* Initialize the argument info vectors */
+  args[0] = &ffi_type_pointer;
+  values[0] = &s;
+
+  /* Initialize the cif */
+  if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, 1,
+        &ffi_type_uint, args) == FFI_OK)
+  {
+    s = "Hello World!";
+    ffi_call(&cif, (void (*)()) puts, &rc, values);
+    s = "This is cool!";
+    ffi_call(&cif, (void (*)()) puts, &rc, values);
+  }
+
   return 0;
 }
